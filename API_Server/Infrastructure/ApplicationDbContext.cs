e3cc8b1f0035ef242bee9652e28804093f6d67a2
Jakub Nowakowski <trynewgame@gmail.com>
2023-11-21 00:57:07 +0100

    Added initial migration, dbcontext and configuration

---
+++ API_Server/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Users;
+using Infrastructure.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class ApplicationDbContext : IdentityDbContext<User>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    }
+
