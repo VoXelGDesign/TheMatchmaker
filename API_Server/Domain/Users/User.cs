a2276e9453cc8e984c2b019a4218f8ac3418f2bd
Jakub Nowakowski <trynewgame@gmail.com>
2023-11-22 16:32:52 +0100

    Decupled User and IdentityUser. removed unnesecary User value objects

--- API_Server/Domain/Users/User.cs
+++ API_Server/Domain/Users/User.cs
@@ -3,8 +3,13 @@ using System.Text.RegularExpressions;
 
 namespace Domain.Users;
 
-public sealed class User : IdentityUser
+public sealed class User
 {
+
+    public UserId Id { get; private set; } = null!;
+    //public UserName Name { get; private set; }
+    public UserSteamProfileLink SteamProfileLink { get; private set; } = null!;
+
 
 }
 
