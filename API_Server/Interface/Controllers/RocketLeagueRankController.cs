aabc12918ae08d328732d0dbe76f2d7cd021f001
VoXelGDesign <trynewgame@gmail.com>
2023-12-14 08:37:50 +0100

    Added new controller for RocketLeague rank

---
+++ API_Server/Interface/Controllers/RocketLeagueRankController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Users.UserAccount.Commands;
+using Application.Users.UserGameRanks.RocketLeague.Commands;
+using Application.Users.UserGameRanks.RocketLeague.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Interface.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class RocketLeagueRankController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public RocketLeagueRankController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<RocketLeagueRankDto>> GetRocketLeagueRank()
+        => await _mediator.Send(new GetUserRocketLeagueRankCommand());
+
+        [HttpPut]
+        public async Task<ActionResult<RocketLeagueUpdateRankDto>> UpdateRocketLeagueRank(RocketLeagueUpdateRankDto dto)
+            => await _mediator.Send(new UpdateRocketLeagueRankCommand(dto));
+
+    }
+}
