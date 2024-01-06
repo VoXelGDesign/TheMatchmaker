aff7bf41924b9ada990392e384e690b7d4e8273d
Jakub Nowakowski <trynewgame@gmail.com>
2024-01-06 13:13:11 +0100

    Create RocketLeagueRankDto.cs

---
+++ Contracts/QueueContracts/RocketLeague/RocketLeagueRankDto.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Games.RocketLeague.Ranks;
+
+namespace Contracts.QueueContracts.RocketLeague;
+
+public record RocketLeagueRankDto(string Name, string Number, string Division)
+{
+    public static RocketLeagueRankDto ParseRocketLeagueRankToDto(RocketLeagueRank rocketLeagueRank)
+    {
+        return new RocketLeagueRankDto(
+            rocketLeagueRank.RocketLeagueRankName.ToString(),
+            rocketLeagueRank.RocketLeagueRankNumber.ToString(),
+            rocketLeagueRank.RocketLeagueDivision.ToString());
+    }
+};
