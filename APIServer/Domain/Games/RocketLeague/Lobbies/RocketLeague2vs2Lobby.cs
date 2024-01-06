dead2284124d8081f36367a381b1de1e482acfed
Jakub Nowakowski <trynewgame@gmail.com>
2024-01-06 13:08:39 +0100

    Added Initial Implementation Of Lobby

---
+++ APIServer/Domain/Games/RocketLeague/Lobbies/RocketLeague2vs2Lobby.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Games.RocketLeague.Palyers;
+
+namespace Domain.Games.RocketLeague.Lobbies;
+public class RocketLeague2vs2Lobby
+{
+    public RocketLeague2vs2LobbyId Id { get; private set; } = null!;
+    public RocketLeaguePlayer Player1 { get; private set; } = null!;
+    public RocketLeaguePlayer Player2 { get; private set; } = null!;
+
+    public RocketLeague2vs2Lobby Create(RocketLeaguePlayer player1, RocketLeaguePlayer player2)
+    => new RocketLeague2vs2Lobby
+    {
+        Id = new RocketLeague2vs2LobbyId(Guid.NewGuid()),
+        Player1 = player1,
+        Player2 = player2
+    };
+
+    public bool AreAllPlayersReady()
+        => Player1.IsReady && Player2.IsReady;
+}
---
+++ APIServer/Domain/Games/RocketLeague/Lobbies/RocketLeague2vs2LobbyId.cs
@@ -0,0 +1,3 @@
+﻿namespace Domain.Games.RocketLeague.Lobbies;
+
+public record RocketLeague2vs2LobbyId(Guid lobbyId);
