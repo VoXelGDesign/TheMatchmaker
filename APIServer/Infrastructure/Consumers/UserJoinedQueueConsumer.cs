67c8299ff267eae3ceb9bf19ca83a97bb07b0fd0
Jakub Nowakowski <trynewgame@gmail.com>
2024-01-17 01:07:36 +0100

    Added states of queue consumers

---
+++ APIServer/Infrastructure/Consumers/UserJoinedQueueConsumer.cs
@@ -0,0 +1,41 @@
+﻿using Contracts.QueueContracts;
+using Domain.Users.User;
+using Domain.Users.UserQueueInfos;
+using Infrastructure.Publishers;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Consumers
+{
+    public class UserJoinedQueueConsumer : IConsumer<UserJoinedQueue>
+    {
+        private readonly ApplicationDbContext _dbContext; 
+        private readonly IQueueStatusChangedPublisher _queueStatusChangedPublisher;
+        public UserJoinedQueueConsumer(ApplicationDbContext dbContext,IQueueStatusChangedPublisher queueStatusChangedPublisher)
+        {
+
+            _dbContext = dbContext;
+            _queueStatusChangedPublisher  = queueStatusChangedPublisher;
+        }
+        public async Task Consume(ConsumeContext<UserJoinedQueue> context)
+        {
+            var guid = Guid.Parse(context.Message.UserIdDto.UserId);
+            var userId = new UserId(guid);
+
+            var userQueueInfo = await _dbContext.UserQueueInfos.FirstOrDefaultAsync(x => x.UserId == userId);
+
+            if(userQueueInfo is null)
+            {
+                userQueueInfo = new UserQueueInfo(userId);
+                _dbContext.UserQueueInfos.Add(userQueueInfo);               
+            }
+
+            userQueueInfo.SetStatusInQueue(context.Message.TimeStamp);
+
+           await _dbContext.SaveChangesAsync();
+
+           await _queueStatusChangedPublisher
+                .PublishAsync(new QueueStatusChanged(context.Message.UserIdDto, QueueStatus.JoinedQueue));
+        }
+    }
+}
---
+++ APIServer/Infrastructure/Consumers/UserRemovedFromQueueConsumer.cs
@@ -0,0 +1,40 @@
+﻿using Contracts.QueueContracts;
+using Domain.Users.User;
+using Domain.Users.UserQueueInfos;
+using Infrastructure.Publishers;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+
+namespace Infrastructure.Consumers
+{
+    public class UserRemovedFromQueueConsumer : IConsumer<UserRemovedFromQueue>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IQueueStatusChangedPublisher _queueStatusChangedPublisher;
+        public UserRemovedFromQueueConsumer(ApplicationDbContext dbContext, IQueueStatusChangedPublisher queueStatusChangedPublisher)
+        {
+            _dbContext = dbContext;
+            _queueStatusChangedPublisher = queueStatusChangedPublisher;
+        }
+        public async Task Consume(ConsumeContext<UserRemovedFromQueue> context)
+        {
+            var guid = Guid.Parse(context.Message.UserIdDto.UserId);
+            var userId = new UserId(guid);
+
+            var userQueueInfo = await _dbContext.UserQueueInfos.FirstOrDefaultAsync(x => x.UserId == userId);
+
+            if (userQueueInfo is null)
+            {
+                userQueueInfo = new UserQueueInfo(userId);
+                _dbContext.UserQueueInfos.Add(userQueueInfo);
+            }
+
+            userQueueInfo.SetStatusNotInQueue(context.Message.TimeStamp);
+
+            await _dbContext.SaveChangesAsync();
+            await _queueStatusChangedPublisher
+                .PublishAsync(new QueueStatusChanged(context.Message.UserIdDto, QueueStatus.LeftQueue));
+        }
+    }
+}
