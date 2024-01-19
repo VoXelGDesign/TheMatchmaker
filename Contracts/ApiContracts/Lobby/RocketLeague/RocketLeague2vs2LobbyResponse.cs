dfb2f7ffc3c0f9226def4502695b4f6b6e9e53da
Jakub Nowakowski <trynewgame@gmail.com>
2024-01-19 17:19:32 +0100

    Added contracts for 2vs2 rocket league lobby

---
+++ Contracts/ApiContracts/Lobby/RocketLeague/RocketLeague2vs2LobbyResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Contracts.ApiContracts.Lobby.RocketLeague;
+
+public record RocketLeague2vs2LobbyResponse
+{
+    public RocketLeaguePlayerDto Player1 { get; set; }
+    public RocketLeaguePlayerDto Player2 { get; set; }
+
+    public RocketLeague2vs2LobbyResponse(RocketLeaguePlayerDto player1, RocketLeaguePlayerDto player2)
+    {
+        Player1 = player1;
+        Player2 = player2; 
+    }
+}
---
+++ Contracts/ApiContracts/Lobby/RocketLeague/RocketLeaguePlayerDto.cs
@@ -0,0 +1,29 @@
+﻿using Contracts.Common;
+
+namespace Contracts.ApiContracts.Lobby.RocketLeague;
+
+public record RocketLeaguePlayerDto
+{
+    public UserIdDto UserIdDto { get; set; }
+    public string UserAccountName { get; set; }
+
+    public string UserAccountSteamProfileLink { get; set; }
+
+    public string DiscordName { get; set; }
+    public bool IsReady { get; set; }
+
+    public RocketLeaguePlayerDto(
+    UserIdDto userIdDto,
+    string userAccountName,
+    string userAccountSteamProfileLink,
+    string discordName,
+    bool isReady)
+    {
+        UserIdDto = userIdDto;
+        UserAccountName = userAccountName;
+        UserAccountSteamProfileLink = userAccountSteamProfileLink;
+        DiscordName = discordName;
+        IsReady = isReady;
+    }
+    
+}
