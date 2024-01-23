2d481b9f3dcadc23afeb099bdae8a700bb194b32
Jakub Nowakowski <trynewgame@gmail.com>
2024-01-23 01:22:15 +0100

    Added consumer for deleting rocket league queue request

---
+++ QueueService/Consumers/DeleteRocketLeagueQueueRequestConsumer.cs
@@ -0,0 +1,28 @@
+﻿using Contracts.QueueContracts.RocketLeague;
+using MassTransit;
+
+namespace QueueService.Consumers;
+
+public class DeleteRocketLeagueQueueRequestConsumer : IConsumer<DeleteRocketLeagueQueueRequestRequest>
+{
+    private readonly IServiceProvider _serviceProvider;
+    public DeleteRocketLeagueQueueRequestConsumer(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task Consume(ConsumeContext<DeleteRocketLeagueQueueRequestRequest> context)
+    {
+        var request = context.Message.UserIdDto;
+
+        var rocketLeagueQueue = _serviceProvider
+            .GetServices<IHostedService>()
+            .OfType<RocketLeagueQueue>()
+            .SingleOrDefault();
+       
+        if (rocketLeagueQueue is null || request.UserId is null)
+            return;
+
+        await rocketLeagueQueue.RemoveFromQueue(request);
+    }
+}
